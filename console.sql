9efb62f03f9f55c02d39e214341025d89b670025
Andrew Jaffie <andrew.jaffie@stonybrook.edu>
2018-02-20 22:33:25 -0500

    Some changes to simplify and clarify the schema

--- console.sql
+++ console.sql
@@ -22,10 +22,6 @@ CREATE TABLE Person(
 CREATE TABLE User( -- user is customer
   Username CHAR(24) NOT NULL,
   PersonID INTEGER NOT NULL ,
-  -- ProfileId,
-  -- we don't need this in user schema,
-  -- just like person hobby example. User and its profile Id are connected by the profile schema with combination of
-  -- userid and profileid as primary key.
 
 
   -- Account number, account creation date same as profile id.
@@ -33,10 +29,8 @@ CREATE TABLE User( -- user is customer
   CHECK (CreditCardNum LIKE '[0-9]{16}'),
   CreditCardExp CHAR(7),
   CHECK (CreditCardExp LIKE '[0-9]{2}/[0-9]{4}'),
-  -- ProfilePlacementPriority CHAR(4) DEFAULT 'dflt',
   Rating DECIMAL,
   LastActiveDate DATE,
-  -- CHECK (ProfilePlacementPriority IN ('High', 'Mid', 'dflt')),
   CHECK (0 <= (SELECT COUNT(*) FROM User) ),
   FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
     ON DELETE NO ACTION
@@ -45,27 +39,24 @@ CREATE TABLE User( -- user is customer
   UNIQUE KEY (Username)
 );
 
-CREATE TABLE Accounts(
+CREATE TABLE Account (
   AccountNum INTEGER NOT NULL ,
   UserID    CHAR(24) NOT NULL ,
   AccountName CHAR(10) DEFAULT 'Default',
   AccountCreationDate DATE NOT NULL ,
-  CHECK ((SELECT COUNT(*) FROM User) <= (SELECT COUNT(*) FROM Accounts)),
+  CHECK ((SELECT COUNT(*) FROM User) <= (SELECT COUNT(*) FROM Account)),
   FOREIGN KEY (UserID) REFERENCES User(Username)
     ON UPDATE CASCADE
     ON DELETE CASCADE ,
   PRIMARY KEY (UserID, AccountNum)
 );
 
-CREATE TABLE Profiles(
+CREATE TABLE Profile (
   ProfileID INTEGER NOT NULL ,
   UserID    CHAR(24) NOT NULL,
   AccountNum INTEGER NOT NULL ,
   ProfileName CHAR(24),
   Age INTEGER,
-  -- Address   CHAR(50),
-  -- City      CHAR(20),
-  -- State     CHAR(15),
   Gender CHAR(10) DEFAULT 'NotToTell',
   Rating DECIMAL,
   CHECK (Gender IN ('NotToTell', 'Male', 'Female')),
@@ -73,97 +64,67 @@ CREATE TABLE Profiles(
   DateGeoRange DECIMAL,
   DateAgeMin INTEGER,
   DateAgeMax INTEGER,
+  Height INTEGER,
+  Weight DECIMAL,
+  HairColor CHAR(25),
+  EyeColor CHAR(20),
+  Race CHAR(20),
   ProfilePlacementPriority CHAR(4) DEFAULT 'dflt',
   CHECK (ProfilePlacementPriority IN ('High', 'Mid', 'dflt')),
   CHECK (DateAgeMin>16),
-  CHECK ((SELECT COUNT(*) FROM User) <= (SELECT COUNT(*) FROM Profiles)),
+  CHECK ((SELECT COUNT(*) FROM User) <= (SELECT COUNT(*) FROM Profile)),
   FOREIGN KEY (UserID) REFERENCES User(Username)
     ON UPDATE CASCADE
     ON DELETE CASCADE ,
-  FOREIGN KEY (AccountNum) REFERENCES Accounts(AccountNum)
+  FOREIGN KEY (AccountNum) REFERENCES Account (AccountNum)
     ON UPDATE CASCADE
     ON DELETE CASCADE ,
   PRIMARY KEY (ProfileID)
 );
 
-CREATE TABLE Hobbies(
+CREATE TABLE Hobby (
   ProfileId INTEGER NOT NULL,
-  Hobby CHAR(20),
-  FOREIGN KEY (ProfileId) REFERENCES Profiles(ProfileID)
+  Hobby CHAR(50),
+  FOREIGN KEY (ProfileId) REFERENCES Profile (ProfileID)
     ON UPDATE CASCADE
     ON DELETE CASCADE ,
   PRIMARY KEY (ProfileId, Hobby)
 );
 
-CREATE TABLE PhysicalChar(
-  ProfileID INTEGER NOT NULL,
-  PhysicalChar CHAR(20),
-  FOREIGN KEY (ProfileID) REFERENCES Profiles(ProfileID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE ,
-  PRIMARY KEY (ProfileID, PhysicalChar)
-);
 
-CREATE TABLE Photos
+
+CREATE TABLE Photo
 (
     ProfileID INTEGER NOT NULL,
     PhotoPath VARCHAR(512),
-    FOREIGN KEY (ProfileID) REFERENCES Profiles (ProfileID)
+    FOREIGN KEY (ProfileID) REFERENCES Profile (ProfileID)
       ON UPDATE CASCADE
       ON DELETE CASCADE ,
     PRIMARY KEY (ProfileID, PhotoPath)
 );
 
 CREATE TABLE Employee(
-  -- EmployeeID INTEGER NOT NULL,
   EmployeeId INTEGER NOT NULL ,
   StartDate DATE,
-  -- HourlyRate DECIMAL,
+  HourlyRate DECIMAL,
   SSN INT,
-  -- LastName CHAR(15) NOT NULL ,
-  -- FirstName CHAR(15) NOT NULL ,
-  -- Address CHAR(50),
-  -- City CHAR(20),
-  -- State CHAR(15),
-  -- ZipCode CHAR(11),
-  -- CHECK (ZipCode LIKE '^[0-9]*$'),
-
-  -- EmployeeType CHAR(5) DEFAULT 'Rglr',
-  -- CHECK (EmployeeType IN ('Mngr', 'Rglr')),
+  EmpType ENUM('Manager', 'Customer Representative') DEFAULT 'Customer Representative' NOT NULL,
   FOREIGN KEY (EmployeeId) REFERENCES Person(PersonID)
     ON UPDATE CASCADE
     ON DELETE CASCADE ,
   PRIMARY KEY (EmployeeId)
 );
 
-CREATE TABLE Manager(
-  ManagerId INTEGER NOT NULL ,
-  HourlyRate DECIMAL NOT NULL ,
-  FOREIGN KEY (ManagerId) REFERENCES Employee(EmployeeId)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE ,
-  PRIMARY KEY (ManagerId)
-);
 
-CREATE TABLE Cust_Rep(
-  Rep_Id INTEGER NOT NULL ,
-  HourlyRate DECIMAL NOT NULL ,
-  FOREIGN KEY (Rep_Id) REFERENCES Employee(EmployeeId)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE ,
-  PRIMARY KEY (Rep_Id)
-);
 
 CREATE TABLE Date(
   DateId INTEGER NOT NULL ,
-  -- User1Id CHAR(24) NOT NULL ,
-  -- User2Id CHAR(24) NOT NULL ,
   ProfileAId INTEGER NOT NULL ,
   ProfileBId INTEGER NOT NULL ,
   CHECK (
     ProfileAId <> ProfileBId AND
-    (SELECT P.UserID FROM Profiles P WHERE ProfileAId = P.ProfileID) <>
-    (SELECT P.UserID FROM Profiles P WHERE ProfileBId = P.ProfileID)
+    (SELECT P.UserID FROM Profile P WHERE ProfileAId = P.ProfileID) <>
+    (SELECT P.UserID FROM Profile P WHERE ProfileBId = P.ProfileID)
   ),
 
   Date DATE,
@@ -176,16 +137,13 @@ CREATE TABLE Date(
   User2Rating INTEGER,
   CHECK(User1Rating IN (1,2,3,4,5)),
   CHECK(User2Rating IN (1,2,3,4,5)),
-  -- CHECK (User1Id <> User2Id),
-  -- FOREIGN KEY (User1Id) REFERENCES User(UserID),
-  -- FOREIGN KEY (User2Id) REFERENCES User(UserID),
-  FOREIGN KEY (ProfileAId) REFERENCES Profiles(ProfileID)
+  FOREIGN KEY (ProfileAId) REFERENCES Profile (ProfileID)
     ON UPDATE CASCADE
     ON DELETE SET NULL ,
-  FOREIGN KEY (ProfileBId) REFERENCES Profiles(ProfileID)
+  FOREIGN KEY (ProfileBId) REFERENCES Profile (ProfileID)
      ON UPDATE CASCADE
      ON DELETE SET NULL ,
-  FOREIGN KEY (CustomerRepresentative) REFERENCES Cust_Rep(Rep_Id)
+  FOREIGN KEY (CustomerRepresentative) REFERENCES Employee(EmployeeId)
      ON UPDATE CASCADE
      ON DELETE NO ACTION ,
   PRIMARY KEY (DateId)
@@ -197,8 +155,8 @@ CREATE TABLE Likes(
   LikeeId INTEGER NOT NULL,
   Date DATE,
   -- self like enabled,
-  FOREIGN KEY (LikerId) REFERENCES Profiles(ProfileID),
-  FOREIGN KEY (LikeeId) REFERENCES Profiles(ProfileID),
+  FOREIGN KEY (LikerId) REFERENCES Profile (ProfileID),
+  FOREIGN KEY (LikeeId) REFERENCES Profile (ProfileID),
   PRIMARY KEY (LikerId, LikeeId)
 );
 
@@ -207,13 +165,13 @@ CREATE TABLE Referral(
   RefereeAId INTEGER NOT NULL ,
   RefereeBId INTEGER NOT NULL,
   Date DATE,
-  FOREIGN KEY (ReferrerId) REFERENCES Profiles(ProfileID)
+  FOREIGN KEY (ReferrerId) REFERENCES Profile (ProfileID)
     ON UPDATE CASCADE
      ON DELETE SET NULL ,
-  FOREIGN KEY (RefereeAId) REFERENCES Profiles(ProfileID)
+  FOREIGN KEY (RefereeAId) REFERENCES Profile (ProfileID)
     ON UPDATE CASCADE
      ON DELETE SET NULL ,
-  FOREIGN KEY (RefereeBId) REFERENCES Profiles(ProfileID)
+  FOREIGN KEY (RefereeBId) REFERENCES Profile (ProfileID)
     ON UPDATE CASCADE
      ON DELETE CASCADE ,
   CHECK (RefereeAId <> RefereeBId <> ReferrerId),
