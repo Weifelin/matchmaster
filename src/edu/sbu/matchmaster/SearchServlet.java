0f56c79806656dac4bcd260d26bce3a77fdaae2b
IanBuitenkant <ian.buitenkant@stonybrook.edu>
2018-05-02 23:30:56 -0400

    searchServlet

--- src/edu/sbu/matchmaster/SearchServlet.java
+++ src/edu/sbu/matchmaster/SearchServlet.java
@@ -13,6 +13,12 @@ public class SearchServlet extends HttpServlet {
         //get values from the fields, the query the database
         UserBean currentUser= (UserBean)request.getAttribute("user");
         String ssn = currentUser.getSsn();
+        System.out.println(ssn);
+        String profileID = request.getParameter("profileID");
+        String minHeight = request.getParameter("minHeight");
+        String maxHeight = request.getParameter("maxHeight");
+        String minWeight = request.getParameter("minWeight");
+        String maxWeight = request.getParameter("maxWeight");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
